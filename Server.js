e2562009e19fc04d97821145fe8012b59b3641d4
Danny <dannydotdev@gmail.com>
2024-11-26 11:40:13 +0100

    Manipulating the server

--- Server.js
+++ Server.js
@@ -1,14 +1,33 @@
 //Creating a server
+//Remove comments to run codes seperately
 import http from "http";
 
+//Test
+
+/*
 const server = http.createServer((request, response) => {
-  response.write("Hello world");
-  //emd the stream - ideally a framework will do this for you
+  response.write("Hello World"); //end the stream - ideally a framework will do this for you
   response.end();
 });
 
-//Now we have to listen on a port
+*/
 
+//Using HTML
+/*
+const server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "text/html"); // Means the content we are inputing is an html file
+  response.statusCode = 300; //Here we can re-configure the status codes
+  response.end("<div> <h1> HTML HEADER TAG </h1></div>");
+});
+*/
+
+//Short form
+const server = http.createServer((request, response) => {
+  response.writeHead(300, { "Content-Type": "text/html" }); //Shorter method of writing the header and status codes
+  response.end("<div> <h1> HTML HEADER TAG </h1></div>");
+});
+
+//Now we have to listen on a port
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
