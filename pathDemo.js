50f4e66583734a9643dec01e9c79d9c47f464677
Danny <dannydotdev@gmail.com>
2024-11-29 11:06:32 +0100

    File path

--- pathDemo.js
+++ pathDemo.js
@@ -10,3 +10,11 @@ console.log(path.dirname(filePath)); //Returns ./dir1/dir2
 
 //extension name
 console.log(path.extname(filePath)); //Returns .txt
+
+//parse
+console.log(path.parse(filePath)); //Returns the path as an object
+
+//Join create a filepath based on argument passed on it [Because delimiters differs on OS]
+
+const filePath2 = path.join("users", "path");
+console.log(filePath2); //Returns users\path ** for windows
