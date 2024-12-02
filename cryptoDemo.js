162a00545e8d29cb500067fb1999c894296e292b
Danny <dannydotdev@gmail.com>
2024-12-02 10:20:55 +0100

    cipher and decipher texts

--- cryptoDemo.js
+++ cryptoDemo.js
@@ -19,3 +19,17 @@ crypto.randomBytes(16, (err, buf) => {
 
 //Encrypting and Decrypting Data
 //Createcipheriv & createDicipheriv
+const algorithm = "aes-256-cbc";
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encryptedData = cipher.update("Hello World", "utf8", "hex");
+encryptedData += cipher.final("hex");
+console.log(encryptedData);
+
+//To deCipher
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decryptedData = decipher.update(encryptedData, "hex", "utf8");
+decryptedData += decipher.final("utf8");
+console.log(decryptedData);
